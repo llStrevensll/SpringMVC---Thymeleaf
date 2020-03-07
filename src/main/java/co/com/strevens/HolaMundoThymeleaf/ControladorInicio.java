7eaa2e61b6319ae93478cc3179dc70b350102d9f
Steven <anguelr12@hotmail.com>
2020-03-06 20:49:18 -0500

    Creacion lista - condicional e iterar en vista

--- src/main/java/co/com/strevens/HolaMundoThymeleaf/ControladorInicio.java
+++ src/main/java/co/com/strevens/HolaMundoThymeleaf/ControladorInicio.java
@@ -1,6 +1,8 @@
 package co.com.strevens.HolaMundoThymeleaf;
 
 import co.com.strevens.domain.Persona;
+import java.util.ArrayList;
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -21,11 +23,25 @@ public class ControladorInicio {
         persona.setApellido("Rodriguez");
         persona.setEmail("correo@corre.com");
         persona.setTelefono("12345678");
+        
+        var persona1 = new Persona();
+        persona1.setNombre("Steven");
+        persona1.setApellido("Rodriguez");
+        persona1.setEmail("correo1@corre.com");
+        persona1.setTelefono("123456789");
+        
+        //var personas = new ArrayList();
+        //personas.add(persona);
+        //personas.add(persona1);
+        
+        var personas = Arrays.asList(persona, persona1);
+        //var personas = new ArrayList();
         log.info("Ejecutando el controlador Spring MVC");
         
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        //model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         
         return "index";
     }
