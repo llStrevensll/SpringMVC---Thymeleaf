b0b8f89ff6aac3235bcb9a3d1645c1c6c385bf97
Steven <anguelr12@hotmail.com>
2020-03-09 19:17:46 -0500

    encriptar password

---
+++ src/main/java/co/com/strevens/util/EncriptarPassword.java
@@ -0,0 +1,19 @@
+package co.com.strevens.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class EncriptarPassword {
+    public static void main(String[] args) {
+        
+        var password = "123";
+        System.out.println("password: " + password);
+        System.out.println("password encriptado: " + encriptarPassword(password));
+
+    }
+    
+    //Encriptar password
+    public static String encriptarPassword(String password){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+}
